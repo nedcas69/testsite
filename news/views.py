5fe7bbc03cb6057b3547c7abedf896cd14ad3895
nedcas69 <nedcas69@github.com>
2022-11-15 11:17:03 +0500

    fff

--- news/views.py
+++ news/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import News
 
 def index(request):
     #print(request)
-    return HttpResponse('Hello world')
+    news = News.objects.all()
+    res = '<h1>Список новостей</h1>'
+    for item in news:
+        res += f'<div>\n<p>{item.title}</p>\n<p>{item.content}</p>\n</div>\n<hr>\n'
+
+
+    return HttpResponse(res)
 
 def test(request):
     #print(request)
