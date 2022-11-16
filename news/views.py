4b38fe4a675ab07b44cd0bc5cfbb14d3b5ec2767
nedcas69 <nedcas69@github.com>
2022-11-16 09:52:40 +0500

    fff

--- news/views.py
+++ news/views.py
@@ -5,13 +5,10 @@ from .models import News
 def index(request):
     #print(request)
     news = News.objects.all()
-    res = '<h1>Список новостей</h1>'
-    for item in news:
-        res += f'<div>\n<p>{item.title}</p>\n<p>{item.content}</p>\n</div>\n<hr>\n'
-
-
-    return HttpResponse(res)
-
-def test(request):
-    #print(request)
-    return HttpResponse('<h1>Тестовая страница</h1>')
+    context = {
+        'news':news, 
+        'title': 'Список новостей'
+        }
+    return render(request, 
+    template_name='news/index.html', 
+    context=context)
